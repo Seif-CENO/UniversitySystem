cea2520c8d66ffc41833792db5072f003a1d0657
Seif Allah Haytham <S.Haytham2483@nu.edu.eg>
2025-12-14 20:11:03 +0200

    Implemented addStudent function with O(1) optimization
    - Added password field to Student struct as per requirements
    - Added tail pointer to SRecords for O(1) insertion performance
    - Implemented addStudent logic ensuring proper tail updates

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 struct Student {
 	int id;
-	std::string name, email, address, phone;
+	std::string name, email, address, phone, password;
 };
 
 class SRecords {
@@ -16,21 +16,35 @@ private:
 		~Node() = default;
 	};
 	Node *head;
+	Node *tail;
 	int nodectr = 0;
 public:
 	SRecords()
-		: head(nullptr) {}
+		: head(nullptr), tail(nullptr) {}
 	~SRecords() {
 		while (head != nullptr) {
 			Node *anchor = head;
 			head = head->next;
 			delete anchor;
 		}
+		tail = nullptr;
 	}
 	
 	// Methods section -> this is where methods are implemented //
+	
+	void addStudent(Student newStudent) {
+		Node *newNode = new Node(newStudent);
+		if (nodectr == 0) {
+			head = tail = newNode;
+		} else {
+			tail->next = newNode;
+			tail = newNode;
+		}
+		++nodectr;
+	}
 };
 
 int main(void) {
-
+	
+	return 0;
 }
