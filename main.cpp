924ed6755398527ddc54702e151cac11e420c165
Seif Allah Haytham <S.Haytham2483@nu.edu.eg>
2025-12-14 21:04:54 +0200

    Implemented robust deleteStudent function handling all edge cases

--- main.cpp
+++ main.cpp
@@ -42,6 +42,43 @@ public:
 		}
 		++nodectr;
 	}
+
+	void deleteStudent(int s_id) {
+		if (nodectr == 0) throw std::out_of_range("Cannot deleteStudent() from an empty Student Record.");
+		else if (nodectr == 1) {
+			if (head->data.id == s_id) {
+				delete head;
+				head = tail = nullptr;
+				--nodectr;
+			} else {
+				std::cout << "Student with ID: " << s_id << " does not exist.\n";
+			}
+			return;
+		} else if (head->data.id == s_id) {
+			Node *anchor = head;
+			head = head->next;
+			delete anchor;
+			--nodectr;
+		} else if (tail->data.id == s_id) {
+			Node *it = head;
+			while (it->next != tail) it = it->next;
+			delete tail;
+			tail = it;
+			tail->next = nullptr;
+			--nodectr;
+		} else {
+			Node *it = head;
+			while (it->next != nullptr && it->next->data.id != s_id) it = it->next;
+			if (it->next == nullptr) {
+				std::cout << "Student with ID: " << s_id << " does not exist.\n";
+			} else {
+				Node *anchor = it->next;
+				it->next = anchor->next;
+				delete anchor;
+				--nodectr;
+			}
+		}
+	}
 };
 
 int main(void) {
